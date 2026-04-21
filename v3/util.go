32dedb66ec49a612c624a0cc6e66855ed506d42e
Elizalde Baguinon <mizmerize@yahoo.com>
2026-04-21 18:15:35 +0800

    Refactor SQL scan destination handling
    Streamline copyScannedToDest and prepareDest logic: simplify error paths by removing reflective assign fallbacks and returning explicit errors for unhandled types; adjust byte/raw handling to use direct assignments and add support for json.RawMessage and []byte cases; update prepareDest to create nullable SQL destinations and include json.RawMessage in destination mapping; remove several assign*/set* reflection helpers; minor formatting and comment additions. These changes simplify type handling and reduce reliance on reflection-based assignment helpers.

--- v3/util.go
+++ v3/util.go
@@ -13,6 +13,7 @@ import (
 )
 
 func copyScannedToDest(dest, src []any) error {
+
 	for i, d := range src {
 		switch x := d.(type) {
 		case *sql.NullString:
@@ -26,7 +27,6 @@ func copyScannedToDest(dest, src []any) error {
 					return errors.New(`unhandled sql.NullString type`)
 				}
 			}
-
 		case *sql.NullByte:
 			if x.Valid {
 				switch s := dest[i].(type) {
@@ -35,12 +35,9 @@ func copyScannedToDest(dest, src []any) error {
 				case **uint8:
 					*s = &x.Byte
 				default:
-					if err := assignUnsignedReflect(dest[i], uint64(x.Byte)); err != nil {
-						return errors.New(`unhandled sql.NullByte type`)
-					}
+					return errors.New(`unhandled sql.NullByte type`)
 				}
 			}
-
 		case *sql.NullInt16:
 			if x.Valid {
 				switch s := dest[i].(type) {
@@ -59,7 +56,6 @@ func copyScannedToDest(dest, src []any) error {
 					}
 				}
 			}
-
 		case *sql.NullInt32:
 			if x.Valid {
 				switch s := dest[i].(type) {
@@ -78,12 +74,9 @@ func copyScannedToDest(dest, src []any) error {
 					ic := int8(x.Int32)
 					*s = &ic
 				default:
-					if err := assignSignedReflect(dest[i], int64(x.Int32)); err != nil {
-						return errors.New(`unhandled sql.NullInt32 type`)
-					}
+					return errors.New(`unhandled sql.NullInt32 type`)
 				}
 			}
-
 		case *sql.NullInt64:
 			if x.Valid {
 				switch s := dest[i].(type) {
@@ -92,12 +85,9 @@ func copyScannedToDest(dest, src []any) error {
 				case **int64:
 					*s = &x.Int64
 				default:
-					if err := assignSignedReflect(dest[i], x.Int64); err != nil {
-						return errors.New(`unhandled sql.NullInt64 type`)
-					}
+					return errors.New(`unhandled sql.NullInt64 type`)
 				}
 			}
-
 		case *sql.NullFloat64:
 			if x.Valid {
 				switch s := dest[i].(type) {
@@ -111,12 +101,9 @@ func copyScannedToDest(dest, src []any) error {
 					xs := float32(x.Float64)
 					*s = &xs
 				default:
-					if err := assignFloatReflect(dest[i], x.Float64); err != nil {
-						return errors.New(`unhandled sql.NullFloat64 type`)
-					}
+					return errors.New(`unhandled sql.NullFloat64 type`)
 				}
 			}
-
 		case *sql.NullBool:
 			if x.Valid {
 				switch s := dest[i].(type) {
@@ -125,12 +112,9 @@ func copyScannedToDest(dest, src []any) error {
 				case **bool:
 					*s = &x.Bool
 				default:
-					if err := assignBoolReflect(dest[i], x.Bool); err != nil {
-						return errors.New(`unhandled sql.NullBool type`)
-					}
+					return errors.New(`unhandled sql.NullBool type`)
 				}
 			}
-
 		case *sql.NullTime:
 			if x.Valid {
 				switch s := dest[i].(type) {
@@ -142,22 +126,21 @@ func copyScannedToDest(dest, src []any) error {
 					return errors.New(`unhandled sql.NullTime type`)
 				}
 			}
-
 		case *[]byte:
 			switch s := dest[i].(type) {
 			case *[]byte:
-				*s = append((*s)[:0], (*x)...)
+				*s = *x
+			case []byte:
+				copy(dest[i].(json.RawMessage), *x)
 			case *json.RawMessage:
-				*s = append((*s)[:0], (*x)...)
+				*s = *x
+			case json.RawMessage:
+				s = *x
 			case **[]uint8:
-				cp := append([]byte(nil), (*x)...)
-				*s = &cp
+				*s = x
 			default:
-				if err := assignBytesReflect(dest[i], *x); err != nil {
-					return errors.New(`unhandled byte type`)
-				}
+				return errors.New(`unhandled byte type`)
 			}
-
 		case *ssd.NullDecimal:
 			switch s := dest[i].(type) {
 			case *ssd.Decimal:
@@ -167,11 +150,10 @@ func copyScannedToDest(dest, src []any) error {
 			default:
 				return errors.New(`unhandled shopspring.NullDecimal type`)
 			}
-
 		case *sql.RawBytes:
 			switch s := dest[i].(type) {
 			case *string:
-				*s = string([]byte(*x))
+				*s = string(([]byte)(*x))
 			case **string:
 				if x == nil || *x == nil {
 					*s = nil
@@ -183,7 +165,7 @@ func copyScannedToDest(dest, src []any) error {
 				}
 				**s = str
 			case *json.RawMessage:
-				*s = append((*s)[:0], (*x)...)
+				*s = ([]byte)(*x)
 			case **json.RawMessage:
 				if x == nil || *x == nil {
 					*s = nil
@@ -195,10 +177,11 @@ func copyScannedToDest(dest, src []any) error {
 					*s = new(json.RawMessage)
 				}
 				**s = rm
+			case json.RawMessage:
+				s = ([]byte)(*x)
 			default:
 				return errors.New(`unhandled sql.RawBytes type`)
 			}
-
 		default:
 			return errors.New(`unhandled sql.Null<type>`)
 		}
@@ -208,44 +191,34 @@ func copyScannedToDest(dest, src []any) error {
 }
 
 func prepareDest(dest []any) (destq []any) {
+
+	// create nullable sql destinations
 	destq = make([]any, len(dest))
 
+	// return values
 	for i, d := range dest {
 		switch x := d.(type) {
 		case *string, **string:
 			destq[i] = &sql.NullString{}
-
 		case *int, *int8, *int32, *uint, *uint32,
 			**int, **int8, **int32, **uint, **uint32:
 			destq[i] = &sql.NullInt32{}
-
 		case *int16, *uint16, **int16, **uint16:
 			destq[i] = &sql.NullInt16{}
-
 		case *int64, *uint64, **int64, **uint64:
 			destq[i] = &sql.NullInt64{}
-
 		case *float32, *float64, **float32, **float64:
 			destq[i] = &sql.NullFloat64{}
-
 		case *bool, **bool:
 			destq[i] = &sql.NullBool{}
-
 		case *time.Time, **time.Time:
 			destq[i] = &sql.NullTime{}
-
-		case []uint8, *[]uint8, **[]uint8, *json.RawMessage:
+		case []uint8, *[]uint8, **[]uint8, *json.RawMessage, json.RawMessage:
 			destq[i] = &[]byte{}
-
 		case *ssd.Decimal, **ssd.Decimal:
 			destq[i] = &ssd.NullDecimal{}
-
 		case *uint8, **uint8:
 			destq[i] = &sql.NullByte{}
-
-		case any, *any:
-			destq[i] = &sql.RawBytes{}
-
 		default:
 			destq[i] = prepareDestReflect(x)
 		}
@@ -268,28 +241,20 @@ func prepareDestReflect(d any) any {
 	switch t.Kind() {
 	case reflect.String:
 		return &sql.NullString{}
-
-	// PostgreSQL smallint targets
 	case reflect.Int8, reflect.Int16, reflect.Uint8, reflect.Uint16:
 		return &sql.NullInt16{}
-
 	case reflect.Int, reflect.Int32, reflect.Uint, reflect.Uint32:
 		return &sql.NullInt32{}
-
 	case reflect.Int64, reflect.Uint64:
 		return &sql.NullInt64{}
-
 	case reflect.Float32, reflect.Float64:
 		return &sql.NullFloat64{}
-
 	case reflect.Bool:
 		return &sql.NullBool{}
-
 	case reflect.Struct:
 		if t == reflect.TypeOf(time.Time{}) {
 			return &sql.NullTime{}
 		}
-
 	case reflect.Slice:
 		if t.Elem().Kind() == reflect.Uint8 {
 			return &[]byte{}
@@ -338,158 +303,6 @@ func setInt16Reflect(v reflect.Value, n int16) error {
 	}
 
 	return fmt.Errorf("cannot assign int16 to %v", v.Type())
-}
-
-func assignSignedReflect(dst any, n int64) error {
-	rv := reflect.ValueOf(dst)
-	if !rv.IsValid() || rv.Kind() != reflect.Ptr || rv.IsNil() {
-		return fmt.Errorf("destination must be a non-nil pointer, got %T", dst)
-	}
-	return setSignedReflect(rv.Elem(), n)
-}
-
-func setSignedReflect(v reflect.Value, n int64) error {
-	if v.Kind() == reflect.Ptr {
-		if v.IsNil() {
-			v.Set(reflect.New(v.Type().Elem()))
-		}
-		return setSignedReflect(v.Elem(), n)
-	}
-
-	switch v.Kind() {
-	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
-		if v.OverflowInt(n) {
-			return fmt.Errorf("value %d overflows %v", n, v.Type())
-		}
-		v.SetInt(n)
-		return nil
-
-	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64, reflect.Uintptr:
-		if n < 0 {
-			return fmt.Errorf("negative value %d cannot be assigned to %v", n, v.Type())
-		}
-		u := uint64(n)
-		if v.OverflowUint(u) {
-			return fmt.Errorf("value %d overflows %v", n, v.Type())
-		}
-		v.SetUint(u)
-		return nil
-	}
-
-	return fmt.Errorf("cannot assign int64 to %v", v.Type())
-}
-
-func assignUnsignedReflect(dst any, n uint64) error {
-	rv := reflect.ValueOf(dst)
-	if !rv.IsValid() || rv.Kind() != reflect.Ptr || rv.IsNil() {
-		return fmt.Errorf("destination must be a non-nil pointer, got %T", dst)
-	}
-	return setUnsignedReflect(rv.Elem(), n)
-}
-
-func setUnsignedReflect(v reflect.Value, n uint64) error {
-	if v.Kind() == reflect.Ptr {
-		if v.IsNil() {
-			v.Set(reflect.New(v.Type().Elem()))
-		}
-		return setUnsignedReflect(v.Elem(), n)
-	}
-
-	switch v.Kind() {
-	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64, reflect.Uintptr:
-		if v.OverflowUint(n) {
-			return fmt.Errorf("value %d overflows %v", n, v.Type())
-		}
-		v.SetUint(n)
-		return nil
-
-	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
-		if n > uint64(^uint64(0)>>1) {
-			return fmt.Errorf("value %d too large for signed assignment to %v", n, v.Type())
-		}
-		i := int64(n)
-		if v.OverflowInt(i) {
-			return fmt.Errorf("value %d overflows %v", n, v.Type())
-		}
-		v.SetInt(i)
-		return nil
-	}
-
-	return fmt.Errorf("cannot assign uint64 to %v", v.Type())
-}
-
-func assignFloatReflect(dst any, f float64) error {
-	rv := reflect.ValueOf(dst)
-	if !rv.IsValid() || rv.Kind() != reflect.Ptr || rv.IsNil() {
-		return fmt.Errorf("destination must be a non-nil pointer, got %T", dst)
-	}
-	return setFloatReflect(rv.Elem(), f)
-}
-
-func setFloatReflect(v reflect.Value, f float64) error {
-	if v.Kind() == reflect.Ptr {
-		if v.IsNil() {
-			v.Set(reflect.New(v.Type().Elem()))
-		}
-		return setFloatReflect(v.Elem(), f)
-	}
-
-	switch v.Kind() {
-	case reflect.Float32, reflect.Float64:
-		if v.OverflowFloat(f) {
-			return fmt.Errorf("value %f overflows %v", f, v.Type())
-		}
-		v.SetFloat(f)
-		return nil
-	}
-
-	return fmt.Errorf("cannot assign float64 to %v", v.Type())
-}
-
-func assignBoolReflect(dst any, b bool) error {
-	rv := reflect.ValueOf(dst)
-	if !rv.IsValid() || rv.Kind() != reflect.Ptr || rv.IsNil() {
-		return fmt.Errorf("destination must be a non-nil pointer, got %T", dst)
-	}
-	return setBoolReflect(rv.Elem(), b)
-}
-
-func setBoolReflect(v reflect.Value, b bool) error {
-	if v.Kind() == reflect.Ptr {
-		if v.IsNil() {
-			v.Set(reflect.New(v.Type().Elem()))
-		}
-		return setBoolReflect(v.Elem(), b)
-	}
-
-	if v.Kind() != reflect.Bool {
-		return fmt.Errorf("cannot assign bool to %v", v.Type())
-	}
-	v.SetBool(b)
-	return nil
-}
-
-func assignBytesReflect(dst any, b []byte) error {
-	rv := reflect.ValueOf(dst)
-	if !rv.IsValid() || rv.Kind() != reflect.Ptr || rv.IsNil() {
-		return fmt.Errorf("destination must be a non-nil pointer, got %T", dst)
-	}
-
-	v := rv.Elem()
-	for v.Kind() == reflect.Ptr {
-		if v.IsNil() {
-			v.Set(reflect.New(v.Type().Elem()))
-		}
-		v = v.Elem()
-	}
-
-	if v.Kind() == reflect.Slice && v.Type().Elem().Kind() == reflect.Uint8 {
-		cp := append([]byte(nil), b...)
-		v.Set(reflect.ValueOf(cp).Convert(v.Type()))
-		return nil
-	}
-
-	return fmt.Errorf("cannot assign []byte to %v", v.Type())
 }
 
 // isInterfaceNil checks if an interface is nil
